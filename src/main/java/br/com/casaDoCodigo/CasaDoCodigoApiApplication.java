3e2af6dda9f7f299f58898a12de50071da0fca6a
Caroline Araujo <ca.araujosouza@gmail.com>
2021-04-14 18:45:42 -0300

    first commit

---
+++ src/main/java/br/com/casaDoCodigo/CasaDoCodigoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.casaDoCodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDoCodigoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDoCodigoApiApplication.class, args);
+	}
+
+}
